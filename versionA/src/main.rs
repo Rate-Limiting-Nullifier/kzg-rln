2c4e83149d70f0d3d74efe57482991bdf2e99d59
Magamedrasul Ibragimov <curryrasul@gmail.com>
2023-04-12 23:18:58 +0400

    chore: add register function

--- versionA/src/main.rs
+++ versionA/src/main.rs
@@ -1,54 +1,108 @@
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+#![allow(clippy::upper_case_acronyms)]
+
 use std::collections::HashMap;
 
 use ark_bls12_381::*;
-use ark_ec::{bls12::Bls12, PairingEngine};
-use ark_poly::univariate::DensePolynomial;
+use ark_ec::PairingEngine;
+use ark_poly::{univariate::DensePolynomial, Polynomial, UVPolynomial};
 use ark_poly_commit::kzg10::*;
 use ark_std::test_rng;
 
+use once_cell::sync::Lazy;
+
 type UniPoly_381 = DensePolynomial<<Bls12_381 as PairingEngine>::Fr>;
+type KZG = KZG10<Bls12_381, UniPoly_381>;
+
+const EPOCH_LIMIT: u8 = 1;
+const DEGREE: usize = EPOCH_LIMIT as usize;
+
+static KEYS: Lazy<(Powers<Bls12_381>, VerifierKey<Bls12_381>)> = Lazy::new(|| {
+    let rng = &mut test_rng();
+    let pp = KZG::setup(DEGREE, true, rng).unwrap();
+
+    let powers_of_g = pp.powers_of_g.clone();
+    let powers_of_gamma_g = vec![pp.powers_of_gamma_g[&0], pp.powers_of_gamma_g[&1]];
+
+    let powers = Powers {
+        powers_of_g: ark_std::borrow::Cow::Owned(powers_of_g),
+        powers_of_gamma_g: ark_std::borrow::Cow::Owned(powers_of_gamma_g),
+    };
+
+    let vk = VerifierKey {
+        g: pp.powers_of_g[0],
+        gamma_g: pp.powers_of_gamma_g[&0],
+        h: pp.h,
+        beta_h: pp.beta_h,
+        prepared_h: pp.prepared_h.clone(),
+        prepared_beta_h: pp.prepared_beta_h.clone(),
+    };
+
+    (powers, vk)
+});
 
 struct RLN {
     limit: u8,
-    trusted_setup_params: UniversalParams<Bls12<Parameters>>,
+    shares: HashMap<Commitment<Bls12_381>, Vec<Fr>>,
 }
 
 impl RLN {
     fn new(limit: u8) -> Self {
         let rng = &mut test_rng();
-        let trusted_setup_params =
-            KZG10::<Bls12_381, UniPoly_381>::setup(limit as usize, false, rng)
-                .expect("Setup failed");
 
         Self {
             limit,
-            trusted_setup_params,
+            shares: HashMap::new(),
         }
     }
-}
 
-struct User {}
+    // fn register()
 
-impl User {
-    fn new() -> Self {
-        Self {}
-    }
-
-    fn send(&self, message: &str) {
+    fn add(&mut self) {
         todo!()
     }
 }
 
+struct User {
+    polynomial: UniPoly_381,
+}
+
+impl User {
+    fn new(degree: usize) -> Self {
+        let rng = &mut test_rng();
+        let polynomial = UniPoly_381::rand(degree, rng);
+
+        Self { polynomial }
+    }
+
+    fn secret(&self) -> Fr {
+        self.polynomial.evaluate(&Fr::from(0))
+    }
+
+    fn register(&self, rln: &mut RLN) {
+        let rng = &mut test_rng();
+        let (comm, rand) = KZG::commit(&KEYS.0, &self.polynomial, None, None).unwrap();
+
+        let proof = KZG::open(&KEYS.0, &self.polynomial, Fr::from(0), &rand).unwrap();
+
+        assert!(KZG::check(&KEYS.1, &comm, Fr::from(0), value, proof));
+    }
+
+    // fn send(&self, message: &str, rln: &mut RLN) {
+    //     rln.add();
+    //     todo!()
+    // }
+}
+
 fn main() {
-    let rln = RLN::new(1);
-    let user = User::new();
+    let mut rln = RLN::new(EPOCH_LIMIT);
+    let user = User::new(DEGREE);
 
-    // user.send("Hello");
-    // user.send("I'm spammer");
+    println!("User's secret is {:?}", user.secret());
 
-    let rng = &mut test_rng();
-    let trusted_setup_params =
-        KZG10::<Bls12_381, UniPoly_381>::setup(1, false, rng).expect("Setup failed");
+    user.register(&mut rln);
 
-    println!("{:#?}", trusted_setup_params);
+    // user.send("Hello", &mut rln);
+    // user.send("I'm spammer", &mut rln);
 }
